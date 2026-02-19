788771f4b441b2d805354c5f8afa1eb1162285f6
Slavenko <slavenko.miljic@nfinnova.com>
2026-02-19 01:01:57 +0100

    Update example app to SwiftUI CounterLabel

--- Example/SMCounterLabel/ViewController.swift
+++ Example/SMCounterLabel/ViewController.swift
@@ -1,67 +1,36 @@
-//
-//  ViewController.swift
-//  SMCounterLabel
-//
-//  Created by Slavenko on 1/26/19.
-//  Copyright © 2019 Slavenko. All rights reserved.
-//
-
 import UIKit
+import SwiftUI
 import SMCounterLabel
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var label: SMCounterLabel!
-    @IBOutlet weak var dummyLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //I recommend using monospaced fonts, since you will get a bit of jitter when animating, since the character width is different on numbers
-        label.font = UIFont(name:"Menlo", size: 50)
-        
-        dummyLabel.font = UIFont(name:"Menlo", size: 50)
-        
-        //Format type integer/decimal/fancy
-        label.formatType = .decimal
-        
-        //How long it takes to animate one character
-        label.duration = 0.6
-        
-        //How long to wait before the next character starts animating
-        label.delay = 0.2
-        
-        //If for some reason you want the animation to slow down towards the end
-        label.durationIncrement = 0.0
-        
-        //Text color obviously
-        label.color = .darkGray
-        
-        //Set the value
-        label.setValue(1234.56)
-        dummyLabel.text = "\(label.getValue())"
-    }
-    
-    @IBAction func updateLabel(_ sender: Any) {
-        let randomNumber = Double.random(min: 1, max: 9999)
-        label.setValue(randomNumber)
-        dummyLabel.text = "\(label.getValue())"
+        let hosting = UIHostingController(rootView: DemoView())
+        addChild(hosting)
+        view.addSubview(hosting.view)
+        hosting.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hosting.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hosting.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hosting.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hosting.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        hosting.didMove(toParent: self)
     }
 }
 
-public extension Double {
-    
-    /// Returns a random floating point number between 0.0 and 1.0, inclusive.
-    public static var random: Double {
-        return Double(arc4random()) / 0xFFFFFFFF
-    }
-    
-    /// Random double between 0 and n-1.
-    ///
-    /// - Parameter n:  Interval max
-    /// - Returns:      Returns a random double point number between 0 and n max
-    public static func random(min: Double, max: Double) -> Double {
-        return Double.random * (max - min) + min
+private struct DemoView: View {
+    @State private var amount: Double = 1234.56
+
+    var body: some View {
+        VStack(spacing: 40) {
+            CounterLabel(value: amount, format: .fancy)
+                .font(.system(size: 50, design: .monospaced))
+                .foregroundStyle(.primary)
+            Button("Random") {
+                amount = Double.random(in: 1...9999)
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
-
